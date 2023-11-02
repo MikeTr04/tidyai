599b720bf04312af7e4bac00f4e60fd96628796e
MikeTr04 <regularinternetuser@gmail.com>
2023-11-02 23:50:16 +0100

    added dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>About this site</h1>
+<p>TODO...</p>
+<a href="/">Home</a>
